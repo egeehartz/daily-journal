99d04af2b2e8830e78438676172795c4d333a21d
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-07-23 16:01:40 -0500

    tried some things

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
 import {journalList} from './pastEntries/journalList.js'
 import { newEntryList } from './newEntry/newEntryList.js'
+import { landingPageBuilder } from './landingPage/pageBuilder.js'
 
-//newEntryList()
+//landingPageBuilder()
 
 newEntry.addEventListener("click", newEntryList)
 pastEntryList.addEventListener("click",journalList)
+home.addEventListener("click",landingPageBuilder)
 
