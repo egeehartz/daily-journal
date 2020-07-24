4d0ed324e68c74105ebc05e74b2a73602185bc86
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-07-24 09:37:35 -0500

    played around with navbar

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,18 @@
 import {journalList} from './pastEntries/journalList.js'
 import { newEntryList } from './newEntry/newEntryList.js'
-import { landingPageBuilder } from './landingPage/pageBuilder.js'
 
-//landingPageBuilder()
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
 
 newEntry.addEventListener("click", newEntryList)
 pastEntryList.addEventListener("click",journalList)
-home.addEventListener("click",landingPageBuilder)
+
+
 
